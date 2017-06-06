d5dbc1e6ade12a5736bf28248fb30d4a04673ccf
Hamish Gray <hamishgray@Hamishs-MacBook-Pro.local>
2017-06-06 14:48:45 +0100

    Progress on building guide pages

--- js/script.js
+++ js/script.js
@@ -197,11 +197,33 @@ function bannerfade(){
 	$('.title-banner__overlay').css({
     "opacity": st / (wh*1.5) + 0.15
 	});
-
 }
 
 $(document).scroll(function() {
 	bannerfade();
+});
+
+
+function overlay_bg(){
+	var st = $(document).scrollTop();
+	var wh = $(window).height();
+
+  $('.js-bg-fade').each(function(){
+
+    var distance = $(this).offset().top;
+    var start = distance - wh;
+    var progress = st - start;
+
+    $(this).find('.js-bg-fade__overlay').css({
+      "opacity": progress / wh
+    });
+
+  });
+
+}
+
+$(document).scroll(function() {
+	overlay_bg();
 });
 
 
